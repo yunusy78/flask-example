72552ba11ea7f1f2a205dfb9c22db34122aa7003
yunusy78 <89937383+yunusy78@users.noreply.github.com>
2023-05-24 20:58:19 +0200

    Update pythonlogintest.py

--- pythonlogintest.py
+++ pythonlogintest.py
@@ -2,14 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 def test_login():
     # Github credentials
     username = "admin"
     password = "admin"
 
     # initialize the Chrome driver with Service object
-    driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options, service_args=['--verbose', '--log-path=/tmp/chromedriver.log'])
+ 
+   chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+   chrome_options = Options()
+   options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+    ]
+    for option in options:
+    chrome_options.add_argument(option)
+    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
     # head to github login page
     driver.get("https://teamdevops.herokuapp.com/login")
     # find username/email field and send the username itself to the input field
