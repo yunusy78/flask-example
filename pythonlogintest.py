d23cb8798ea960ff0855432053a6f283e1f11ff1
yunusy78 <89937383+yunusy78@users.noreply.github.com>
2023-05-24 18:36:01 +0200

    Update pythonlogintest.py

--- pythonlogintest.py
+++ pythonlogintest.py
@@ -2,36 +2,43 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-# Github credentials
-username = "admin"
-password = "admin"
 
-# initialize the Chrome driver
-driver = webdriver.Chrome()
-# head to github login page
-driver.get("https://teamdevops.herokuapp.com/login")
-# find username/email field and send the username itself to the input field
-driver.find_element("name", "id").send_keys(username)
-# find password input field and insert password as well
-driver.find_element("name", "pw").send_keys(password)
+def test_login():
+    # Github credentials
+    username = "admin"
+    password = "admin"
 
-# Bekleme süresini artırın (örneğin 10 saniye)
-wait = WebDriverWait(driver, 10)
+    # initialize the Chrome driver with Service object
+    driver = webdriver.Chrome(service=webdriver.chrome.service.Service(executable_path="C:/Users/yunus/anaconda3/Scripts/chromedriver"))
+    # head to github login page
+    driver.get("https://teamdevops.herokuapp.com/login")
+    # find username/email field and send the username itself to the input field
+    driver.find_element("name", "id").send_keys(username)
+    # find password input field and insert password as well
+    driver.find_element("name", "pw").send_keys(password)
 
-# Öğe yüklenene kadar bekleyin
-submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit'].btn.btn-success")))
-submit_button.click()
-WebDriverWait(driver=driver, timeout=10).until(
-    lambda x: x.execute_script("return document.readyState === 'complete'")
-)
-error_message = "Incorrect username or password."
-# get the errors (if there are)
-errors = driver.find_elements("css selector", ".flash-error")
-# print the errors optionally
-# for e in errors:
-#     print(e.text)
-# if we find that error message within errors, then login is failed
-if any(error_message in e.text for e in errors):
-    print("[!] Login failed")
-else:
-    print("[+] Login successful")
+    # Bekleme süresini artırın (örneğin 10 saniye)
+    wait = WebDriverWait(driver, 10)
+
+    # Öğe yüklenene kadar bekleyin
+    submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit'].btn.btn-success")))
+    submit_button.click()
+
+    # Bekleme süresini artırın (örneğin 10 saniye)
+    WebDriverWait(driver=driver, timeout=10).until(
+        lambda x: x.execute_script("return document.readyState === 'complete'")
+    )
+
+    error_message = "Incorrect username or password."
+    # get the errors (if there are)
+    errors = driver.find_elements("css selector", ".flash-error")
+    # if we find that error message within errors, then login is failed
+    if any(error_message in e.text for e in errors):
+        print("[!] Login failed")
+        assert False  # Login başarısız olduğunda testi başarısız olarak işaretleyin
+    else:
+        print("[+] Login successful")
+        assert True  # Login başarılı olduğunda testi başarılı olarak işaretleyin
+
+    # Tarayıcıyı kapatın
+    driver.quit()
