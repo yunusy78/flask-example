9ec98644c268c219489c5ac23ac12b4ffcb8c3c5
yunusy78 <89937383+yunusy78@users.noreply.github.com>
2023-05-25 21:05:07 +0200

    Update pythonlogintest.py

--- pythonlogintest.py
+++ pythonlogintest.py
@@ -1,55 +1,54 @@
+from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 def test_login():
     # Github credentials
-    username = "admin"
-    password = "admin"
+    username = "admin1"
+    password = "admin1"
 
-    # initialize the Chrome driver with Service object
-    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-    chrome_options = Options()
-    options = [
-        "--headless",
-        "--disable-gpu",
-        "--window-size=1920,1200",
-        "--ignore-certificate-errors",
-        "--disable-extensions",
-        "--no-sandbox",
-        "--disable-dev-shm-usage"
-    ]
-    for option in options:
-        chrome_options.add_argument(option)
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    try:
+        # initialize the Chrome driver with Service object
+        driver = webdriver.Chrome(service=webdriver.chrome.service.Service(executable_path="C:/Users/yunus/anaconda3/Scripts/chromedriver"))
+        # head to github login page
+        driver.get("https://teamdevops.herokuapp.com/login")
+        # find username/email field and send the username itself to the input field
+        driver.find_element("name", "id").send_keys(username)
+        # find password input field and insert password as well
+        driver.find_element("name", "pw").send_keys(password)
 
-    # head to github login page
-    driver.get("https://teamdevops.herokuapp.com/login")
-    # find username/email field and send the username itself to the input field
-    driver.find_element("name", "id").send_keys(username)
-    # find password input field and insert password as well
-    driver.find_element("name", "pw").send_keys(password)
-    wait = WebDriverWait(driver, 10)
-    submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit'].btn.btn-success")))
-    submit_button.click()
+        wait = WebDriverWait(driver, 10)
 
-    WebDriverWait(driver=driver, timeout=10).until(
-        lambda x: x.execute_script("return document.readyState === 'complete'")
-    )
+        submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[type='submit'].btn.btn-success")))
+        submit_button.click()
 
-    error_message = "Incorrect username or password."
-    # get the errors (if there are)
-    errors = driver.find_elements("css selector", ".flash-error")
-    # if we find that error message within errors, then login is failed
-    if any(error_message in e.text for e in errors):
-        print("[!] Login failed")
-        assert False  # Login
-    else:
-        print("[+] Login successful")
-        assert True  # Login
-    driver.quit()
+        WebDriverWait(driver=driver, timeout=10).until(
+            lambda x: x.execute_script("return document.readyState === 'complete'")
+        )
+
+        error_message = "Incorrect username or password."
+        # get the errors (if there are)
+        errors = driver.find_elements("css selector", ".flash-error")
+        # if we find that error message within errors, then login is failed
+        if any(error_message in e.text for e in errors):
+            print("[!] Login failed")
+            assert False
+        else:
+            print("[+] Login successful")
+
+            # Check if Admin Dashboard button is present
+            admin_dashboard_button = wait.until(EC.presence_of_element_located((By.XPATH, "//a[@href='/admin/']")))
+            if admin_dashboard_button.is_displayed():
+                print("[+] Admin Dashboard button found")
+                assert True
+            else:
+                print("[!] Admin Dashboard button not found")
+                assert False
+
+    except Exception as e:
+        print("[!] An error occurred during login:", str(e))
+        assert False
+
+    finally:
+        driver.quit()
